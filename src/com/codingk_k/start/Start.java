0e114a384239dca86b459b4a210c08e1ad908d84
knight <1300684594@qq.com>
2017-05-16 00:36:57 +0800

    完成出个界面UI

---
+++ src/com/codingk_k/start/Start.java
@@ -0,0 +1,9 @@
+package com.codingk_k.start;
+
+import com.codingk_k.ui.MainPage;
+
+public class Start {
+	public static void main(String[] args) {
+		MainPage mainPage = new MainPage();
+	}
+}
---
+++ src/com/codingk_k/ui/MainPage.java
@@ -0,0 +1,100 @@
+package com.codingk_k.ui;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class MainPage extends JFrame implements ActionListener {
+	private JTextField tf_result;
+	private JPanel pl_button;
+
+	private JButton bt_ce;
+	private JButton bt_back;
+	private JButton bt_ans;
+	private JButton bt_mul;
+	private JButton bt_div;
+	private JButton bt_sub;
+	private JButton bt_add;
+	private JButton bt_eql;
+	private JButton bt_dot;
+	private JButton bt_db0;
+	private JButton[] bt_num;
+
+	public MainPage() {
+		init();
+		setSize(400, 600);
+		draw();
+		addActionListener();
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		this.setTitle("计算器");
+		this.setVisible(true);
+	}
+
+	public void init() {
+		setLayout(new BorderLayout());
+		tf_result = new JTextField("0.");
+		tf_result.setHorizontalAlignment(JTextField.RIGHT);
+
+		pl_button = new JPanel();
+		pl_button.setLayout(new GridLayout(5, 4));
+
+		bt_ce = new JButton("C");
+		bt_back = new JButton("<-");
+		bt_ans = new JButton("ans");
+		bt_add = new JButton("+");
+		bt_sub = new JButton("-");
+		bt_mul = new JButton("*");
+		bt_div = new JButton("/");
+		bt_db0 = new JButton("00");
+		bt_dot = new JButton(".");
+		bt_eql = new JButton("=");
+		bt_num = new JButton[10];
+		for (int i = 0; i < 10; i++) {
+			bt_num[i] = new JButton(String.valueOf(i));
+		}
+	}
+
+	public void draw() {
+		add(tf_result, BorderLayout.NORTH);
+
+		pl_button.add(bt_ce);
+		pl_button.add(bt_back);
+		pl_button.add(bt_ans);
+		pl_button.add(bt_div);
+		pl_button.add(bt_num[7]);
+		pl_button.add(bt_num[8]);
+		pl_button.add(bt_num[9]);
+		pl_button.add(bt_mul);
+		pl_button.add(bt_num[4]);
+		pl_button.add(bt_num[5]);
+		pl_button.add(bt_num[6]);
+		pl_button.add(bt_sub);
+		pl_button.add(bt_num[1]);
+		pl_button.add(bt_num[2]);
+		pl_button.add(bt_num[3]);
+		pl_button.add(bt_add);
+
+		pl_button.add(bt_db0);
+		pl_button.add(bt_num[0]);
+		pl_button.add(bt_dot);
+		pl_button.add(bt_eql);
+		add(pl_button, BorderLayout.CENTER);
+	}
+
+	public void addActionListener() {
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+	}
+}
